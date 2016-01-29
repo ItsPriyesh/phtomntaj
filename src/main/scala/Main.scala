7b73c544dee546dde8ab4cee6213cecd259bd27d
Priyesh Patel <priyeshpatel.96@gmail.com>
2016-01-29 10:16:08 -0500

    Decode arg to bitmap

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
+import android.graphics.{Bitmap, BitmapFactory}
+
 object Main {
-  def main(args: Array[String]) {
-    print("lulz")
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) print("No file specified. Please pass the path to an image as an argument.")
+    else Option(BitmapFactory.decodeFile(args(0))) match {
+      case Some(bitmap) => doStuff(bitmap)
+      case None => print("Unable decode file as an image.")
+    }
+  }
+
+  def doStuff(bitmap: Bitmap): Unit = {
+
   }
 }
